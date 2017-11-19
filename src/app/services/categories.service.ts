4477dda5335e0e354e7d0a2fa4677f0f37f3cfdb
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2017-11-19 21:45:16 +0800

    Add categories.

--- src/app/services/categories.service.ts
+++ src/app/services/categories.service.ts
@@ -11,7 +11,18 @@ export class CategoriesService
   public getAllCategories()
   {
     let categories = new Array<Category>();
+    categories.push(new Category(1, 'countries', '国'));
+    categories.push(new Category(1, 'expressions', '感情表現'));
+    categories.push(new Category(1, 'family', '家族'));
     categories.push(new Category(1, 'food', '食品'));
+    categories.push(new Category(1, 'greetings', '挨拶'));
+    categories.push(new Category(1, 'majors', '専攻'));
+    categories.push(new Category(1, 'money', 'お金'));
+    categories.push(new Category(1, 'numbers', '番号'));
+    categories.push(new Category(1, 'occupations', '仕事'));
+    categories.push(new Category(1, 'places', '位置'));
+    categories.push(new Category(1, 'things', '物'));
+    categories.push(new Category(1, 'words that point', '単語の趣旨'));
     return categories;
   }
 }
