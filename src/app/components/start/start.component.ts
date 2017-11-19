682ad9c993636b31f20dab2bbd5c50a1ac51d276
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2017-11-19 21:45:32 +0800

    Code StartComponent HTML.

--- src/app/components/start/start.component.ts
+++ src/app/components/start/start.component.ts
@@ -2,11 +2,28 @@
 // Angular modules
 import { Component, OnInit } from '@angular/core';
 
+// Services
+import { CategoriesService } from '../../services/categories.service'
+import { Category } from '../../models/category.model';
+
 @Component({
   selector: 'start',
   templateUrl: './start.component.html',
   styleUrls: ['./start.component.css']
 })
-export class StartComponent {
+export class StartComponent implements OnInit {
+  private categories: Array<Category>;
+  
+  constructor
+  (
+    private categoriesService: CategoriesService
+  )
+  {
+  }
+
+  ngOnInit()
+  {
+    this.categories = this.categoriesService.getAllCategories();
+  }
 
 }
