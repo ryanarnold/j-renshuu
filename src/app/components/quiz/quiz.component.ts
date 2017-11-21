d53806163c90769601189a83d69f94311539e714
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2017-11-22 07:14:49 +0800

    Write basic functionality of QuizComponent.

--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -2,17 +2,40 @@
 // Angular modules
 import { Component, OnInit } from '@angular/core';
 
+// Services
+import { QuizService } from '../../services/quiz.service';
+import { Word } from '../../models/word.model';
+import { WordsService } from '../../services/words.service';
+
 @Component({
   selector: 'quiz',
   templateUrl: './quiz.component.html',
   styleUrls: ['./quiz.component.css']
 })
-export class QuizComponent implements OnInit {
+export class QuizComponent implements OnInit
+{
+  private words: Array<Word>;
+  private currentWord: Word;
+  private currentWordIndex: number;
   
-  constructor()
+  constructor
+  (
+    private quizService: QuizService,
+    private wordsService: WordsService
+  )
   { }
 
+  nextWord()
+  {
+    this.currentWordIndex += 1;
+    this.currentWord = this.words[this.currentWordIndex];
+  }
+
   ngOnInit()
-  { }
+  {
+    this.words = this.wordsService.getByCategory(this.quizService.getCategories());
+    this.currentWordIndex = -1;
+    this.nextWord();
+  }
 
 }
