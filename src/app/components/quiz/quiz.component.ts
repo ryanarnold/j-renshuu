a3d3c055d2fdafb206fe32ce88ad6ea8602b401d
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2017-11-26 12:47:32 +0800

    Write HTML for QuizComponent.

--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -17,6 +17,7 @@ export class QuizComponent implements OnInit
   private words: Array<Word>;
   private currentWord: Word;
   private currentWordIndex: number;
+  private userAnswer: string;
   
   constructor
   (
@@ -27,15 +28,26 @@ export class QuizComponent implements OnInit
 
   nextWord()
   {
+    if (this.userAnswer == this.currentWord.definition)
+    {
+      this.quizService.incrementCorrect();
+    }
+    else
+    {
+      this.quizService.incrementWrong();
+    }
+
     this.currentWordIndex += 1;
     this.currentWord = this.words[this.currentWordIndex];
+    this.userAnswer = '';
   }
 
   ngOnInit()
   {
     this.words = this.wordsService.getByCategory(this.quizService.getCategories());
-    this.currentWordIndex = -1;
-    this.nextWord();
+    this.currentWordIndex = 0;
+    this.currentWord = this.words[this.currentWordIndex];
+    this.userAnswer = '';
   }
 
 }
