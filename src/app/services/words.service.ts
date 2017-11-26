e10c13c55bdd079867e82140a155bed566621cdd
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2017-11-26 15:39:54 +0800

    Shuffle words.

--- src/app/services/words.service.ts
+++ src/app/services/words.service.ts
@@ -84,6 +84,23 @@ export class WordsService
       }
     }
 
+    words = this.shuffleWordsArray(words);
+
     return words;
+  }
+
+  private shuffleWordsArray(array: Array<Word>)
+  {
+    let j, x, i;
+
+    for (i = array.length - 1; i > 0; i--)
+    {
+      j = Math.floor(Math.random() * (i + 1));
+      x = array[i];
+      array[i] = array[j];
+      array[j] = x;
+    }
+
+    return array;
   }
 }
